f7ce351fd94201f68c6be3bed80edfb177a415a0
Dylan Anthony <dbanty@users.noreply.github.com>
2023-01-31 17:59:49 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sample-rust-action
+
+An example GitHub Action written in Rust, generated from [dbanty/rust-github-action-template](https://github.com/dbanty/rust-github-action-template). You can either "Use this template" and customize it yourself, or `cargo generate dbanty/rust-github-action-template` to create a new repo from this template.
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::fs::write;
+use std::process::exit;
+
+#[tokio::main]
+async fn main() {
+    let github_output_path = env::var("GITHUB_OUTPUT").unwrap();
+
+    let args: Vec<String> = env::args().collect();
+    let error = &args[1];
+
+    if !error.is_empty() {
+        eprintln!("Error: {error}");
+        write(github_output_path, format!("error={error}")).unwrap();
+        exit(1);
+    }
+}
